c4d4ceb12a2d911bd525734bc79ddf4e7ee3bda6
aou101 <wodnrdlgla@gmail.com>
2022-05-11 20:32:35 +0900

    10. 점수계산

--- AA.py
+++ AA.py
@@ -1,28 +1,17 @@
 import sys
-sys.stdin=open("input.txt", "rt")
+#sys.stdin=open("input.txt", "rt")
 
 n = int(input())
-a = list(map(int, input().split()))
+res = list(map(int, input().split()))
+tot = 0
+sum = 0
 
-def reverse(x):
-    res = 0
-    while x>0:
-        t = x % 10
-        res = res * 10 + t
-        x = x // 10
-    return res
 
-def isPrime(x):
-    if x==1:
-        return False
-    for i in range(2, x//2+1):
-        if x % i == 0:
-            return False
+for i in range(n):
+    if res[i] == 1:
+        tot += 1
+        sum += tot
     else:
-        return True
+        tot = 0
 
-
-for x in a:
-    res = reverse(x)
-    if(isPrime(res)):
-        print(res)
+print(sum)
